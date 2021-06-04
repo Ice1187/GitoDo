ea3e1fc6bbf7b8ddce7d490627b4447bf9d652fd
Ice1187 <Icing1187@gmail.com>
2021-06-04 13:20:10 +0800

    feat: basic branch demo

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.css';
 import Header from '../components/header';
 import MainTaskView from '../components/mainTaskView';
 import Footer from '../components/footer';
+import BranchCanvas from '../components/branchCanvas';
 
 export default function Home() {
   return (
@@ -16,8 +17,9 @@ export default function Home() {
       <Header></Header>
 
       <main className={styles.main + ' bg-gray-100 relative'}>
+        <BranchCanvas ></BranchCanvas>
         <div className='sm:top-28 top-24 lg:right-7 right-2 lg:left-80 left-20 px-10 absolute w-auto'>
-          <div className='container flex flex-row mx-auto items-center'>
+          <div className='flex flex-row mx-auto items-center'>
             <h1 className='text-2xl font-semibold'>Task</h1>
             <div className='flex-grow' />
           </div>
